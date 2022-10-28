c81306019ee9f8120cfc09b8f6c919f4fedf33c8
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-29 00:22:08 +0200

    u

--- im2scene/giraffe/models/generator.py
+++ im2scene/giraffe/models/generator.py
@@ -87,9 +87,9 @@ class Generator(nn.Module):
                 not_render_background=False,
                 only_render_background=False):
         if latent_codes is None:
-            #latent_codes = self.get_latent_codes(gaze_direction = gaze_direction, batch_size = batch_size)
+            latent_codes = self.get_latent_codes( batch_size = batch_size)
             latent_codes = torch.cat((data.get('code')[:,:100],data.get('code')[:,-2:]), dim=1).double().to(self.device), torch.ones([batch_size, self.z_dim_bg]).double().to(self.device),torch.cat((data.get('code')[:,179:279],data.get('code')[:,-2:]),dim=1).double().to(self.device) ,torch.ones([batch_size, self.z_dim_bg]).double().to(self.device)
-
+        print(torch.cat((data.get('code')[:,:100],data.get('code')[:,-2:]), dim=1).double().to(self.device).dtype)
         if camera_matrices is None:
             #camera_matrices = self.get_random_camera(batch_size)
             #camera_matrices = data.get('cam_mat').to(self.device), data.get('world_mat').to(self.device)
@@ -125,18 +125,20 @@ class Generator(nn.Module):
             n_boxes = 1
         return n_boxes
 
-    def get_latent_codes(self, gaze_direction, batch_size=2, tmp=1.):
+    def get_latent_codes(self, gaze_direction=None, batch_size=2, tmp=1.):
         z_dim, z_dim_bg = self.z_dim, self.z_dim_bg
 
         n_boxes = self.get_n_boxes()
-        gaze_direction = torch.reshape(input = gaze_direction, shape = (batch_size,1,2) )
+        #gaze_direction = torch.reshape(input = gaze_direction, shape = (batch_size,1,2) )
 
         def sample_z(x): return self.sample_z(x, tmp=tmp)
         z_shape_obj = sample_z((batch_size, n_boxes, z_dim))
-        z_shape_obj[:,:,-2:] = gaze_direction
+        #z_shape_obj[:,:,-2:] = gaze_direction
         z_app_obj = sample_z((batch_size, n_boxes, z_dim))
         z_shape_bg = sample_z((batch_size, z_dim_bg))
         z_app_bg = sample_z((batch_size, z_dim_bg))
+
+        print(z_app_obj.dtype)
 
         return z_shape_obj, z_app_obj, z_shape_bg, z_app_bg
 
